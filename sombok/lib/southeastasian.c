5de0de2ca1e7154e1480fb9ef4eeaa985358d2c1
hatukanezumi <hatuka@nezumi.nu>
2011-03-13 01:10:19 +0000

    2011.3  Sun Mar 13 2011 * configure.ac: unichar_t may be unsigned int or unsigned long. * include/sombok.h: now generated from include/sombok.h.in. * Bug Fix: southeastasian.c: th_wbrk() may return 0 _or_ -1 when   breaking position was not found. * break.c: some optimizations.

--- sombok/lib/southeastasian.c
+++ sombok/lib/southeastasian.c
@@ -42,7 +42,7 @@ void linebreak_southeastasian_flagbreak(gcstring_t * gcstr)
 {
 #ifdef USE_LIBTHAI
     wchar_t *buf;
-    size_t i, j, k;
+    size_t i, j, len;
     int brk, sa;
 
     if (gcstr == NULL || gcstr->gclen == 0)
@@ -51,32 +51,41 @@ void linebreak_southeastasian_flagbreak(gcstring_t * gcstr)
 	  LINEBREAK_OPTION_COMPLEX_BREAKING))
 	return;
 
-    /* Copy string to temp buffer so that abuse of external module avoided. */
-    if ((buf = malloc(sizeof(wchar_t) * (gcstr->len + 1))) == NULL)
-	return;
-    for (i = 0; i < gcstr->len; i++)
-	buf[i] = gcstr->str[i];
-    buf[i] = (wchar_t) 0;
-    k = i;
+    len = gcstr->len;
 
-    /* Flag breaking points. */
+    /* Copy string to temp buffer so that abuse of external module avoided. */
+    if ((buf = malloc(sizeof(wchar_t) * (len + 1))) == NULL)
+	return;
+#ifdef SOMBOK_UNICHAR_T_IS_WCHAR_T
+    memcpy(buf, gcstr->str, sizeof(wchar_t) * len);
+#else /* SOMBOK_UNICHAR_T_IS_WCHAR_T */
+    for (i = 0; i < len; i++)
+	buf[i] = (wchar_t) (gcstr->str[i]);
+#endif /* SOMBOK_UNICHAR_T_IS_WCHAR_T */
+    buf[len] = (wchar_t) 0;
+
+    /*
+     * Flag breaking points.
+     * Note: th_wbrk() sometimes returns -1 when breaking positions weren't
+     * found.
+     */
     sa = 0;
-    for (i = 0, j = 0; buf[j] && th_wbrk(buf + j, &brk, 1); j += brk) {
+    for (i = 0, j = 0; j < len && th_wbrk(buf + j, &brk, 1) == 1; j += brk) {
 	/* check if external module is broken. */
 	assert(0 <= brk);
 	assert(brk != 0);
-	assert(brk < k);
+	assert(brk < len);
 
 	for (; i < gcstr->gclen && gcstr->gcstr[i].idx <= j + brk; i++) {
 	    /* check if external module broke temp buffer. */
-	    assert(buf[i] == gcstr->str[i]);
+	    assert(buf[i] == (wchar_t) (gcstr->str[i]));
 
 	    if (gcstr->gcstr[i].lbc == LB_SA) {
 		if (!sa)
 		    /* skip the first grapheme of each SA block. */
 		    sa = 1;
 		else if (gcstr->gcstr[i].flag)
-		    /* already flagged by _preprocess(). */
+		    /* already flagged by _prep(). */
 		    ;
 		else if (linebreak_lbclass(gcstr->lbobj,
 					   gcstr->str[gcstr->gcstr[i].idx -
